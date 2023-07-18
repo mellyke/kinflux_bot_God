7bf6c36b6bcf691dccc21cb54cc540e470df046a
mellyke <139506088+mellyke@users.noreply.github.com>
2023-07-18 19:47:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254110077535', 'zamzack254', true],
-  ['254110077535', 'kinflux digital', true], 
+  [254768005355 ', 'melly', true],
+  [254768005355 ', 'melly', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254110077535','254110377776'] 
-global.prems = ['254110077535', '254110377776', '254765777423']
+global.mods = [254768005355 ',254768005355'] 
+global.prems = [254768005355', 254768005355', 254768005355']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
